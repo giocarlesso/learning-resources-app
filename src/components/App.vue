a5beeb2fd7f82092e212ea3238e19e7089e6f5f2
Giocarlesso <carlessogio@gmail.com>
2021-01-12 18:50:59 -0300

    Main component created and some dummy data added

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,30 @@
 <template>
-  <ul></ul>
+  <ul v-for="resource in storedResources" :key="resource.id">
+    {{
+      resource.title
+    }}
+  </ul>
 </template>
 
 <script>
 export default {
   data() {
-    return { storedResources: [] };
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Offial Guide',
+          descriptio: 'The official Vue JS documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          descriptio: 'The official Google searchpage',
+          link: 'https://google.com'
+        }
+      ]
+    };
   }
 };
 </script>
